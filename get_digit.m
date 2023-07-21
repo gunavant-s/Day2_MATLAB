43285915be1f2f2b2139c4630e77136ae89808cf
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-21 07:58:57 +0530

    Create get_digit.m

---
+++ get_digit.m
@@ -0,0 +1,14 @@
+function f = get_digit()
+vrow1 = [0:9]
+cond = 1
+while cond == 1
+    var = input('Enter the value : ')
+    if (var <= 9) && (var >= 0)
+        fprintf('The value is %d.\n',var)
+        cond = 0
+    else
+        disp('Invalid output. Enter again.\n')
+    end
+end
+end
+
