233de3627b7dc7cddf2ee11bebd7dad435e268e4
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-22 12:07:21 +0530

    Update get_digit.m

--- get_digit.m
+++ get_digit.m
@@ -1,13 +1,13 @@
 function f = get_digit()
-vrow1 = [0:9]
-cond = 1
+vrow1 = [0:9];
+cond = 1;
 while cond == 1
-    var = input('Enter the value : ')
+    var = input('Enter the value : ');
     if (var <= 9) && (var >= 0)
-        fprintf('The value is %d.\n',var)
-        cond = 0
+        fprintf('The value is %d.\n',var);
+        cond = 0;
     else
-        disp('Invalid output. Enter again.\n')
+        disp('Invalid output. Enter again.\n');
     end
 end
 end
